35f5784874c231908b0e6af069d8acf7bbf94a7c
mathav18 <cmathavan34@gmail.com>
2023-01-07 15:27:29 +0530

    hello

---
+++ script.js
@@ -0,0 +1,14 @@
+// function timer(){
+  
+//     for(let i=60;i>0;i--){
+      
+      
+      
+//     }
+    
+
+// }
+// function display(x){
+//     let play=document.getElementById("display");
+//     play.innerText=x;
+// }
